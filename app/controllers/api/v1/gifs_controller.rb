01c73c02251ae53a739877f23f553e1177d54787
Michael Xu <xu.michaelxia@gmail.com>
2018-04-08 17:14:30 +0800

    finished edit and show endpoints

--- app/controllers/api/v1/gifs_controller.rb
+++ app/controllers/api/v1/gifs_controller.rb
@@ -2,4 +2,28 @@ class Api::V1::GifsController < Api::V1::BaseController
   def index
     @gifs = Gif.all
   end
+
+  def show
+    @gif = Gif.find(params[:id])
+  end
+
+  def update
+    @gif = Gif.find(params[:id])
+    if @gif.update(gif_params)
+      render :show
+    else
+      render_error
+    end
+  end
+
+  private
+
+  def gif_params
+    params.require(:gif).permit(:tags, :author, :image, :collected)
+  end
+
+  def render_error
+    render json: { errors: @gif.errors.full_messages },
+      status: :unprocessable_entity
+  end
 end
---
+++ app/views/api/v1/gifs/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @gif, :id, :tags, :author, :image, :collected
