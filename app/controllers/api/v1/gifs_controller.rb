b6c2ac499d45146e7f7926679e2af985cc6446e7
Michael Xu <xu.michaelxia@gmail.com>
2018-04-08 17:38:51 +0800

    endpoint of new and delete done

--- app/controllers/api/v1/gifs_controller.rb
+++ app/controllers/api/v1/gifs_controller.rb
@@ -16,6 +16,21 @@ class Api::V1::GifsController < Api::V1::BaseController
     end
   end
 
+  def create
+    @gif = Gif.new(gif_params)
+    if @gif.save
+      render :show, status: :created
+    else
+      render_error
+    end
+  end
+
+  def destroy
+    @gif = Gif.find(params[:id])
+    @gif.destroy
+    head :no_content
+  end
+
   private
 
   def gif_params
