78d76cee83bfa7d900e967961c44d9f5c751df20
Lauren Emms <lauren.emms@live.co.uk>
2018-04-16 17:57:14 +0800

    Added collection controller, actions, routes and views

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -2,4 +2,12 @@ class Collection < ApplicationRecord
   belongs_to :user
   belongs_to :gif
   validates_uniqueness_of :gif_id, :scope => [ :user_id ]
+
+  def user_name
+    user.name
+  end
+
+  def user_avatar
+    user.avatar
+  end
 end
