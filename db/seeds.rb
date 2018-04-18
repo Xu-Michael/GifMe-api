1cf6100bb366c5a277c92d28d23324bde0413ad5
Michael Xu <xu.michaelxia@gmail.com>
2018-04-18 17:01:28 +0800

    added dynamic ids for seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+Collection.destroy_all
+Gif.destroy_all
+User.destroy_all
+
 5.times do
   user = User.create(name: Faker::Name.name, avatar: Faker::Avatar.image)
   puts "Generated user #{user.name}"
@@ -43,42 +47,42 @@ seed = [
 
 seed.each do |gif|
   puts "Generating user #{gif[:user_id]}'s gif..."
-  new_gif = Gif.create(image: gif[:image], user_id: rand(1..5))
+  new_gif = Gif.create(image: gif[:image], user_id: User.all.sample.id)
   puts "Generating a collection with Gif: #{new_gif.id}..."
   Collection.create(user_id: new_gif.user_id, gif_id: new_gif.id)
 end
 
-a = Gif.find(1)
+a = Gif.first
 a.tag_list = "secret, gossip, shh"
 a.save
 puts "1. tag list = #{a.tag_list}"
 
-b = Gif.find(2)
+b = Gif.find(a.id + 1)
 b.tag_list = "funny, laugh, flash"
 b.save
 puts "2. tag list = #{b.tag_list}"
 
-c = Gif.find(3)
+c = Gif.find(b.id + 1)
 c.tag_list = "funny, trump, shake"
 c.save
 puts "3. tag list = #{c.tag_list}"
 
-d = Gif.find(4)
+d = Gif.find(c.id + 1)
 d.tag_list = "cat, typing, funny"
 d.save
 puts "4. tag list = #{d.tag_list}"
 
-e = Gif.find(5)
+e = Gif.find(d.id + 1)
 e.tag_list = "cat, funny, money"
 e.save
 puts "5. tag list = #{e.tag_list}"
 
-f = Gif.find(6)
+f = Gif.find(e.id + 1)
 f.tag_list = "baby, funny, spoon"
 f.save
 puts "6. tag list = #{f.tag_list}"
 
-g = Gif.find(7)
+g = Gif.find(f.id + 1)
 g.tag_list = "dog, dance, funny"
 g.save
 puts "7. tag list = #{g.tag_list}"
