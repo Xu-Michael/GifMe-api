75bd7842cfcad18c3153436026e7187d15e5fef9
Michael Xu <xu.michaelxia@gmail.com>
2018-04-24 17:44:31 +0800

    redo dependant destroy on collection model

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,6 +1,6 @@
 class Collection < ApplicationRecord
   belongs_to :user
-  belongs_to :gif, dependent: :destroy
+  belongs_to :gif
   validates_uniqueness_of :gif_id, :scope => [ :user_id ]
 
   def user_name
